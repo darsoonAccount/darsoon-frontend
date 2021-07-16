3e05f6d2b13c492daa59c087d283a3780ab7a029
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-07-16 15:38:52 -0400

    add teachers page

---
+++ pages/teachers/apply.tsx
@@ -0,0 +1,12 @@
+import Form from "../../components/Forms";
+
+export default function Apply () {
+    return (
+        <Form>
+            <label>Name</label>
+            <input></input>
+            <label>Family Name</label>
+            <input></input>
+        </Form>
+    );
+}
---
+++ pages/teachers/index.tsx
@@ -0,0 +1,13 @@
+export default function teachers() {
+    return (
+        <>
+        <h2>Teachers</h2>
+        <p>
+            <a href="/teachers/apply">
+            Become a teacher
+
+            </a>
+        </p>
+        </>
+    );
+}
