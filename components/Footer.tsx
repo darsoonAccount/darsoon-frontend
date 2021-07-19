6345523ec84c7c3bdc2bb258285d000643c1f091
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-07-19 14:03:49 -0400

    create Layout with header and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Styled from "styled-components";
+import { themeVars } from "./GlobalStyles";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <h3 className="site-title">
+        Darsoon
+      </h3>
+      <div className="links-in-footer">
+        <div className="flex-column">
+          <h4>About Us</h4>
+          <p>Who we are</p>
+          <p>Our values</p>
+        </div>
+        <div className="flex-column">
+          <h4>Contact Us</h4>
+          <p>Customer Care</p>
+          <p>Be an instructor</p>
+        </div>
+        <div className="flex-column">
+          <h4>Terms of Service</h4>
+          <p>Privacy Policy</p>
+          <p>Cookies</p>
+        </div>
+
+        <div className="flex-column">
+          <h4>Social Media</h4>
+          <p>Instagram</p>
+          <p>Twitter</p>
+        </div>
+      </div>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
+
+const StyledFooter = Styled.footer`
+flex: 0;
+padding-top: 1rem;
+background: ${themeVars.darkColor};
+
+.site-title {
+  font-weight: 900;
+  font-size: 1.5em;
+  span {
+    color: white;
+  }
+}
+
+.links-in-footer {
+padding: 2rem;
+}
+
+h3 {
+  padding:2rem;
+  }
+
+div {
+color: white;
+padding: 1rem;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+}
+h4 {
+  padding: 1rem;
+  color: white;
+}
+
+.flex-column {
+  h4 {
+    padding: 0;
+    text-align: left;
+    margin-bottom: 0.5rem;
+  }
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+}
+
+.footnote {
+  padding-inline: 2rem;
+  padding-block: 3rem;
+  margin-top: 1rem;
+  flex-direction: column;
+  text-align: center;
+  color: white;
+  background: #1d132b;
+}
+
+`;
---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+import React, { useContext } from "react";
+import Link from "next/link";
+import Styled from "styled-components";
+import { themeVars } from "./GlobalStyles";
+
+// import ProfileInHeader from "./auth/ProfileInHeader";
+// import { AppContext } from "./AppProvider";
+// import { useAuth } from "./auth/AuthProvider";
+
+const Header = () => {
+  // const { currentUser } = useAuth();
+
+  return (
+    <>
+      <StyledHeader>
+        <nav>
+          <h1 className="site-title">
+            <Link href="/">Darsoon</Link>
+          </h1>
+          <Link href="/teachers">Teachers</Link>
+          <Link href="/classes">Classes</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+
+        {/* <ProfileInHeader /> */}
+      </StyledHeader>
+    </>
+  );
+};
+
+export default Header;
+
+const StyledHeader = Styled.header`
+flex: 0;
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+background: ${themeVars.primaryColor};
+/* padding: 1rem; */
+min-height: 6rem;
+padding-inline: 1rem ;
+font-weight: 900;
+
+.site-title {
+  font-weight: 900;
+  font-size: 1.5em;
+}
+
+nav {
+    padding: 1rem;
+    color: white;
+    display: flex;
+align-items: center;
+    gap: 2rem;
+     
+     a {
+       color: white;
+       text-decoration: none;
+
+     }
+}
+`;
