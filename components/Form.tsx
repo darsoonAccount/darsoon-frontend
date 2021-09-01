3ac1b07a4d0dfcf2bcf1a8df80bbd82ee2c32529
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-01 15:52:01 -0400

    add toast notification for form submit errors

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useContext } from "react";
 import Styled from "styled-components";
-import { AppContext, useApi } from "../contexts/AppProvider";
+import { AppContext, useApi, useNotif } from "../contexts/AppProvider";
 import { useAuth } from "../contexts/AuthProvider";
 import { themeVars } from "./GlobalStyles";
 import Loading from "./Loading";
@@ -18,35 +18,32 @@ export default function Form({ children, url, method, buttonText, handleDataAfte
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState(null);
   const { api } = useApi();
+  const { notify } = useNotif();
 
   const submitHandler = (event) => {
     event.preventDefault();
     setIsLoading(true);
     setMessage(null);
 
-    //put form data inside a Object:
+    //collectdata from form and put it inside an object:
     const formData = {};
     const formElement = event.target;
     const formInputs = Array.from(formElement.querySelectorAll("input, textarea"));
     formInputs.forEach((inputElement) => {
-      console.log(inputElement.type);
       if (inputElement.type === "number") {
-        
         formData[inputElement.name] = Number(inputElement.value);
       } else {
         formData[inputElement.name] = inputElement.value;
       }
     });
 
-    console.log("Form Data", formData);
     api
       .post(url, formData)
       .then((res) => {
         handleDataAfterSuccess(res.data.data);
       })
       .catch((error) => {
-        setMessage(error.response.data.message);
-        console.log("Error Request!!!", error.request, "Error Response!!!", error.response);
+        notify(error.message, "error");
       });
     setIsLoading(false);
   };
@@ -57,7 +54,9 @@ export default function Form({ children, url, method, buttonText, handleDataAfte
         <div className="inputs">{children}</div>
         {isLoading && <Loading />}
         {message && <p className="message">{message}</p>}
-        <button type="submit">{buttonText ?? "Submit"}</button>
+        <button className="button" type="submit">
+          {buttonText ?? "Submit"}
+        </button>
       </StyledFrom>
     </>
   );
