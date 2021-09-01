ac916313cee50f0286a46e993d25c7cab705e2f9
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-01 16:11:16 -0400

    simplify admin dashboard page

--- pages/admin-dashboard/index.tsx
+++ pages/admin-dashboard/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
-import TeacherApplicationsPanel from "../../components/adminDashboardComponents/TeacherApplicationsPanel";
-import AdminDashNav from "../../layouts/AdminDashNav";
-import { themeVars } from "../../components/GlobalStyles";
 import AdminDashLayout from "../../layouts/adminDashLayout";
 
 export default function AdminDashboard() {
   return (
     <Div>
-      <h1>Admin Dashboard!</h1>
+      <p>You are in Admin Dashboard.</p>
     </Div>
   );
 }
 AdminDashboard.Layout = AdminDashLayout;
 
-const Div = styled.div``;
+const Div = styled.div`
+  display: grid;
+  place-items: center;
+`;
