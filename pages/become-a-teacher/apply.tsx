074a8e70c8f2a3ff0ecfbdb96152aba2acdc6c17
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-03 11:27:29 -0400

    move applicationForm to seperate page

---
+++ pages/become-a-teacher/apply.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import ApplicationForm from "../../components-teacher-applicant/ApplicationForm";
+import ApplicationSubmittedBox from "../../components-teacher-applicant/ApplicationSubmittedBox";
+
+export default function ApplyPage({ myProps }) {
+  const [isSubmitedSuccefully, setIsSubmitedSuccefully] = useState(false);
+
+  const handleDataAfterSuccess = (data) => {
+    setIsSubmitedSuccefully(true);
+  };
+
+  return (
+    <Div>
+      {isSubmitedSuccefully ? (
+        <>
+          <ApplicationSubmittedBox />
+        </>
+      ) : (
+        <>
+          <p>لطفا فرم زیر را پر نمایید</p>
+          <ApplicationForm handleDataAfterSuccess={handleDataAfterSuccess} isTwoColumns={true} />
+        </>
+      )}
+    </Div>
+  );
+}
+const Div = styled.div`
+  padding: 1.5rem;
+  border-radius: 1.5rem;
+  background: ghostwhite;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  display: grid;
+  place-items: center;
+`;
