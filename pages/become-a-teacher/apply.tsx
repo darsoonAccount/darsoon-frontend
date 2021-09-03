b62ead6575017cf4301124df25da3ce3442c1735
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-03 18:25:09 -0400

    fix bug

--- pages/become-a-teacher/apply.tsx
+++ pages/become-a-teacher/apply.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ApplicationForm from "../../components-teacher-applicant/ApplicationForm";
 import ApplicationSubmittedBox from "../../components-teacher-applicant/ApplicationSubmittedBox";
