bb40839f5407a9f7110bb9b6077f3d006f1fe298
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-03 11:27:44 -0400

    add previous applications panel

--- pages/become-a-teacher/index.tsx
+++ pages/become-a-teacher/index.tsx
@@ -1,52 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { useAuth } from "../../contexts/AuthProvider";
 import LoginBox from "../../components/LoginBox";
 import InfoBox from "../../components/InfoBox";
 import { useRouter } from "next/router";
-import { getRouteMatcher } from "next/dist/next-server/lib/router/utils";
-import { useState } from "react";
-import ApplicationSubmittedBox from "../../components-teacher-applicant/ApplicationSubmittedBox";
-import ApplicationForm from "../../components-teacher-applicant/ApplicationForm";
+import { useApi } from "../../contexts/AppProvider";
+import Loading from "../../components/Loading";
+import Row from "../../components/Row";
+import Link from "next/link";
+import Panel from "../../components/Panel";
+import Rows from "../../components/Rows";
 
 export default function BecomeATeacherPage() {
   const { loggedInUser } = useAuth();
+  const { api } = useApi();
+  const [previousApplications, setPreviousApplications] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [isSubmitedSuccefully, setIsSubmitedSuccefully] = useState(false);
-
-  const handleDataAfterSuccess = (data) => {
-    setIsSubmitedSuccefully(true);
-  };
+  useEffect(() => {
+    if (loggedInUser) {
+      setIsLoading(true);
+      const userId = loggedInUser.userId;
+      api
+        .get(`api/j/teacherApplications?applicantUserId=${userId}`)
+        .then((res) => {
+          console.log("here res", res);
+          setPreviousApplications(res.data.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log("here err", err);
+          setPreviousApplications(null);
+          setIsLoading(false);
+        });
+    }
+  }, [loggedInUser]);
 
   return (
     <Page>
-      {!isSubmitedSuccefully ? (
+      {!loggedInUser ? (
         <>
-          {!loggedInUser && (
-            <>
-              <InfoBox>Please create an account first. If you allready have an account, Log in to your account.</InfoBox>
-              <LoginBox redirectTo="/become-a-teacher" />
-            </>
-          )}
-          {loggedInUser && (
-            <>
-              <p>لطفا فرم زیر را پر نمایید</p>
-              <ApplicationForm handleDataAfterSuccess={handleDataAfterSuccess} />
-            </>
-          )}
+          <InfoBox>Please create an account first. If you allready have an account, Log in to your account.</InfoBox>
+          <LoginBox redirectTo="/become-a-teacher" />
         </>
       ) : (
-        <ApplicationSubmittedBox />
+        <>
+          <Panel>
+            <h2>درخواست‌های پیشین</h2>
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <>
+                {previousApplications && previousApplications.length > 0 ? (
+                  <Rows>
+                    {previousApplications.map((app) => {
+                      return (
+                        <Row>
+                          <p>firstname: {app.firstname}</p>
+                          <p>lastname: {app.lastname}</p>
+                          <p>status: {app.status}</p>
+                        </Row>
+                      );
+                    })}
+                  </Rows>
+                ) : (
+                  <p>شما تا کنون درخواستی صبت نکرده‌اید</p>
+                )}
+              </>
+            )}
+          </Panel>
+            <Link href="/become-a-teacher">
+              <a className="primary-button">ایجاد درخواست جدید</a>
+            </Link>
+        </>
       )}
     </Page>
   );
 }
 const Page = styled.div`
-  padding: 1.5rem;
+  padding: 2rem;
+  margin: 0 auto;
   display: grid;
-  /* place-content: center; */
-  grid-template: 1fr / 1fr; 
+  grid-template: 1fr / 1fr;
+
   gap: 1rem;
   width: 100%;
+  max-width: 1000px;
 `;
