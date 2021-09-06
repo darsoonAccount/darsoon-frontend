12d8427bfe878d5ecc0a0c2107efc817ae0996bf
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-05 22:30:36 -0400

    add Layout property to nextComponent Type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import GloabalLayout from "../layouts/GloabalLayout";
 import AuthProvider from "../contexts/AuthProvider";
 import AppProvider from "../contexts/AppProvider";
 import LanguageProvider, { useLang } from "../contexts/LangProvider";
+import type { nextComponentWithLayout } from "../types/nextComponentWithLayout";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+//add Layout property to components in order to apply nested layout
+type ModifiedAppProps = AppProps & {
+  Component: nextComponentWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: ModifiedAppProps) {
   const Layout = Component.Layout || EmptyLayout;
 
   return (
