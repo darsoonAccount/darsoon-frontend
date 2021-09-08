89d2d3ec1bede64972a7b60941c70891e32ccca4
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-07 21:30:27 -0400

    init PageWrapper component

---
+++ components/layout/PageWrapper.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function PageWrapper({ children }) {
+  return <StyledPageWrapper>{children}</StyledPageWrapper>;
+}
+const StyledPageWrapper = styled.div`
+  padding-inline: min(10vw, 2rem);
+  padding-block: min(10vh, 5rem);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
