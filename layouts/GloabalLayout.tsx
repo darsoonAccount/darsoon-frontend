156a4e5abf5286845a838698ba4a1b1eadd2ea6e
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-08 16:04:06 -0400

    move global Head tags to layout form home page

--- layouts/GloabalLayout.tsx
+++ layouts/GloabalLayout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import styled from "styled-components";
@@ -12,6 +13,19 @@ export default function GloabalLayout({ children }) {
 
   return (
     <>
+      <Head>
+        <meta name="description" content="Dasroon is platform to find and get classes for your kids" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/fonts/woff2/IRANSansWeb_Black.woff2" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/woff2/IRANSansWeb_Bold.woff2" as="font" crossOrigin="" />
+        {/* <link rel="preload" href="/fonts/woff2/IRANSansWeb_Light.woff2" as="font" crossOrigin="" />  */}
+        {/* <link rel="preload" href="/fonts/woff2/IRANSansWeb_Medium.woff2" as="font" crossOrigin="" /> */}
+        {/* <link rel="preload" href="/fonts/woff2/IRANSansWeb_UltraLight.woff2" as="font" crossOrigin="" /> */}
+        <link rel="preload" href="/fonts/woff2/IRANSansWeb.woff2" as="font" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;900&display=swap" rel="stylesheet"></link>
+      </Head>
       <GlobalStyles />
       {lang === "fa" && <RTLStyles />}
       <Header />
