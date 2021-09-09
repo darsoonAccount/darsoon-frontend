862000c16c92c764a3f840ee1c846d6c54615c5e
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-09 12:26:17 -0400

    add Fa support

--- components-teacher-applicant/ApplicationSubmittedBox.tsx
+++ components-teacher-applicant/ApplicationSubmittedBox.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import En from "../components/translation/En";
+import Fa from "../components/translation/Fa";
 
 export default function ApplicationSubmittedBox() {
   return (
     <Div>
-      <h1>Your Application is successfully submited</h1>
-      <p>We will reach out to you if you are good match</p>
+      <h1>
+      <En>Your Application is successfully submited</En>
+      <Fa>درخواست شما با موفقیت ثبت شد.</Fa>
+
+      </h1>
+      <p>
+        <En>We will reach out to you if you are good match</En>
+        <Fa>در صورتی که بتوانیم با شما همکاری کنیم با شما تماس خواهیم گرفت.</Fa>
+      </p>
     </Div>
   );
 }
 const Div = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
 `;
