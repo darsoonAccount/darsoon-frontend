1511d1e15fe4aa9860cd6d523f63a7a7713a7d7c
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-09 07:46:59 -0400

    improve Tr

--- components-admin-dashboard/Tr.tsx
+++ components-admin-dashboard/Tr.tsx
@@ -1,36 +1,60 @@
 import React from "react";
 import styled from "styled-components";
+import En from "../components/translation/En";
+import Fa from "../components/translation/Fa";
 import { themeVars } from "../styles/GlobalStyles";
 
-export default function Tr(props) {
-  const { name, isTextArea } = props;
+interface Iprops {
+  labelEn: string;
+  labelFa?: string;
+  name: string;
+  isHidden?: boolean;
+  placeholderEn?: string;
+  placeholderFa?: string;
+  isTextarea?: boolean;
+  defaultValue?: any;
+  [key: string]: any;
+}
+
+export default function Tr(props: Iprops) {
+  const { name, labelFa, labelEn, isTextArea, defaultValue } = props;
   // const inputProps = props;
   // if (inputProps.isText) delete inputProps.isTextArea;
   return (
     <StyledTr>
       <th>
-        <label>{name}</label>
+        <label>
+          <Fa>{labelFa}</Fa>
+          <En>{labelEn}</En>
+        </label>
       </th>
-      <td>{isTextArea ? <textarea className="input" rows="4" {...props}></textarea> : <input className="input" {...props} />}</td>
+      <td>{isTextArea ? <textarea className="input" rows={4} {...props}></textarea> : <input name={name} defaultValue={defaultValue} className="input" {...props} />}</td>
     </StyledTr>
   );
 }
 const StyledTr = styled.tr`
+display: flex;
+justify-content: stretch;
   th {
-    padding: 1rem;
-    background: aliceblue;
+    padding: 0.3rem;
     text-align: start;
     display: table-cell;
     vertical-align: middle;
-    width: 1%;
+    width: 20rem;
     white-space: nowrap;
     height: auto;
+    text-align: end;
+    vertical-align: middle;
+    
+    label {
+      vertical-align: middle;
+      text-align: end;
+    }
   }
   td {
     height: auto;
-    width: auto;
-    padding: 1rem;
-    background: lightsteelblue;
+    width: 40rem;
+    padding: 0.3rem;
     text-align: start;
     color: dimgray;
     display: table-cell;
@@ -40,16 +64,16 @@ const StyledTr = styled.tr`
   .input {
     width: 100%;
     display: block;
-    border: none;
+    border: 1px solid lightgray;
     background: ghostwhite;
     backdrop-filter: opacity(100%);
     padding-inline: 0.7rem;
-    padding-block: 0.6rem;
-    border-radius: ${themeVars.borderRadius};
+    padding-block: 0.1rem;
+    border-radius: 0.3rem;
     color: black;
   }
 
-  teaxtarae {
+  textarea {
     min-height: 8rem;
     resize: vertical;
     overflow-y: auto;
