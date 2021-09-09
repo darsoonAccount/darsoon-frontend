74750ffa663f87da6f2b416f03d51c8be08b8536
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-09 12:25:54 -0400

    improve teacher Application form

--- components-teacher-applicant/ApplicationForm.tsx
+++ components-teacher-applicant/ApplicationForm.tsx
@@ -30,10 +30,11 @@ export default function ApplicationForm({ handleDataAfterSuccess, isTwoColumns }
 
   return (
     <Form url="/api/teacherApplication/add" handleDataAfterSuccess={handleDataAfterSuccess} isTwoColumns={isTwoColumns}>
-      <p>
+      <h2 className="span  ">
         <En>User Information</En>
         <Fa>اطلاعات کاربر</Fa>
-      </p>
+      </h2>
+      {/* here should be another form to complete persoanl data */}
       <TextInput name="country" labelEn="Country" labelFa="کشور محل سکونت" />
       <TextInput name="city" labelEn="city" labelFa="شهر محل سکونت" />
 
@@ -41,12 +42,12 @@ export default function ApplicationForm({ handleDataAfterSuccess, isTwoColumns }
       <TextInput name="linkedin" labelEn="linkedin" labelFa="صفحه لینکدین" />
       <TextInput name="website" labelEn="website" labelFa="وبسایت شخصی" />
       <TextInput name="nameOfRefrencePerson" labelEn="refrence" labelFa="نام معرف" />
-      <p>
+      <h2 className="span">
         <En>Classes you want to teach:</En>
         <Fa>کلاسی که می‌خواهید درس بدهید</Fa>
-      </p>
+      </h2>
 
-      <Select name="topics" labelEn="topics" labelFa="شاخه تخصصی">
+      <Select name="topicId" labelEn="Topic" labelFa="شاخه تخصصی">
         {topics && topics.length > 0 ? (
           <>
             <En>
@@ -73,40 +74,57 @@ export default function ApplicationForm({ handleDataAfterSuccess, isTwoColumns }
       </Select>
       <TextInput name="expertiseName" labelEn="Expertise" labelFa="موضوع کلاس" />
       <TextInput name="productName" labelEn="Product (Class) name" labelFa="نام کلاس" />
-
       <TextInput name="pricePerSession" type="number" step="1" labelEn="Price" labelFa="هزینه هر جلسه (به دلار کانادا)" />
       <TextInput name="sessionDuration" type="number" step="5" labelEn="Session Duration" labelFa="طول زمانی هر جلسه (به دقیقه)" />
       <Select name="ageGroup" labelEn="Age Group" labelFa="گروه سنی مناسب">
         <Fa>
-          <option>خردسال (۴ تا ۶ سال)</option>
-          <option>کودک (۷ تا ۱۲ سال)</option>
-          <option>نوجوان (۱۳ تا ۱۸ سال)</option>
-          <option>بزرگسال (۱۹ تا ۶۴ سال)</option>
-          <option>ارشد (۶۵ سال به بالا)</option>
+          <option value="Child (4 to 6 years)">خردسال (۴ تا ۶ سال)</option>
+          <option value="Child (4 to 6 years)">کودک (۷ تا ۱۲ سال)</option>
+          <option value="Teenager (13 to 18 years)">نوجوان (۱۳ تا ۱۸ سال)</option>
+          <option value="Adult (19 to 64 years)">بزرگسال (۱۹ تا ۶۴ سال)</option>
+          <option value="Senior (over 65 years)">ارشد (۶۵ سال به بالا)</option>
         </Fa>
         <En>
-          <option>Child (4 to 6 years)</option>
-          <option>Child (6 to 12 years)</option>
-          <option>Teenager (13 to 18 years)</option>
-          <option>Adult (19 to 64 years)</option>
-          <option>Senior (over 65 years)</option>
+          <option value="Child (4 to 6 years)">Child (4 to 6 years)</option>
+          <option value="Child (4 to 6 years)">Child (4 to 6 years)</option>
+          <option value="Teenager (13 to 18 years)">Teenager (13 to 18 years)</option>
+          <option value="Adult (19 to 64 years)">Adult (19 to 64 years)</option>
+          <option value="Senior (over 65 years)">Senior (over 65 years)</option>
         </En>
       </Select>
-      <p>
-        {/* <En>Education and Experiences</En> */}
+      <h2 className="span">
+        <En>Education and Experiences</En>
         <Fa>سابقه کاری و تحصیلی شما</Fa>
-      </p>
+      </h2>
+      <Select name="englishFluency" labelEn="English Fluency" labelFa="میزان تسلط بر زبان انگلیسی">
+        <Fa>
+          <option value="Low (only able to teach in Farsi)">کم (فقط توانایی تدریس به فارسی‌زبان)</option>
+          <option value="Intermediate (Able to teach to those who can not speak Farsi, but understand Farsi)">متوسط (توانایی شنیدن و استفاده از لغات انگلیسی در میان مکالمه فارسی، توانایی تدریس به کسی که فارسی نمی‌تواند صحبت کند اما متوجه می‌شود)</option>
+          <option value="High (Able to speak and understand English and able to teach in English to non-Farsi speakers)">عالی (توانایی کامل شنیدن و صحبت کردن انگلیسی، توانایی تدریس به زبان انگلیسی به غیرفارسی زبان)</option>
+        </Fa>
+        <En>
+          <option value="Low (only able to teach in Farsi)">Low (Only able to teach in Farsi)</option>
+          <option value="Intermediate (Able to teach to those who can not speak Farsi, but understand Farsi)">Intermediate (Able to teach to those who can not speak Farsi, but understand Farsi)</option>
+          <option value="High (Able to speak and understand English and able to teach in English to non-Farsi speakers)">High (Able to speak and understand English and able to teach in English to non-Farsi speakers)</option>
+        </En>
+      </Select>
       <TextInput name="levelOfEducation" labelEn="Level of Education" labelFa="مدرک تحصیلی" />
       <TextInput name="university" labelEn="University" labelFa="دانشگاه محل تحصیل" />
+      <h3 className="span">
+        <En>More Details</En>
+        <Fa>جزييات بیشتر</Fa>
+      </h3>
       <TextArea name="inPersonTeachingExperience" labelEn="In Person Teaching Experience" labelFa="تجربه تدریس حضوری (مثلا پنج سال تدرس در آموزشگاه خوارزمی)" />
       <TextArea name="onlineTeachingExperience" labelEn="Online Teaching Experience" labelFa="تجربه تدریس آنلاین (دو سال تدریس آنلاین به حدود 20 شاگرد)" />
       <TextArea name="abroadTeachingExperience" labelEn="Teaching Experience outsie of Iran" labelFa="تجربه تدریس به ایرانیان خارج از کش ور (مثلا شش ماه تدریس گیتار به سه بچه دوزبانه در کانادا)" />
       <TextArea name="applicantNotes" labelEn="Any other piece of information that would help us on reviewing your application" labelFa="هر مطلب دیگری که دانستن آن برای ما مفید است" />
       <TextInput name="applicantUserId" labelEn="Applicant User Id" labelFa="شناسه کاربر درخواست کننده" defaultValue={loggedInUser?.userId} isHidden={true} />
-      <button className="button" type="submit">
-        <En>Submit</En>
-        <Fa>ارسال درخواست</Fa>
-      </button>
+      <div className="span">
+        <button className="button" type="submit">
+          <En>Submit</En>
+          <Fa>ارسال درخواست</Fa>
+        </button>
+      </div>
     </Form>
   );
 }
