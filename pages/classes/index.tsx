67d2b35329ce3ea450a6ac2b1b72571c42572922
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-09 10:47:56 -0400

    init ClassesPage

---
+++ pages/classes/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import ClassCard from "../../components/ClassCard";
+import Cards from "../../components/layout/Cards";
+import PageWrapper from "../../components/layout/PageWrapper";
+
+export default function ClassesPage({ products }) {
+  return (
+    <StyledPageWrapper>
+      <h1>Here is a list of classes</h1>
+      <Cards>
+        {products &&
+          products.map((product) => {
+            return <ClassCard product={product} />;
+          })}
+      </Cards>
+    </StyledPageWrapper>
+  );
+}
+const StyledPageWrapper = styled(PageWrapper)`
+  width: 100%;
+`;
+
+export const getServerSideProps = async () => {
+  const res = await fetch(`https://darsoon.uc.r.appspot.com/api/product`);
+  const json = await res.json();
+  const products = json.data;
+
+  return {
+    props: { products },
+  };
+};
